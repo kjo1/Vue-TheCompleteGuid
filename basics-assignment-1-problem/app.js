0e83a215903b97d05358f004bf02333879814943
kjo <kjo0@outlook.com>
2020-12-08 21:33:43 -0500

    Assignment  1: Data Binding

---
+++ basics-assignment-1-problem/app.js
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      name: "My name",
+      age: 20,
+      //   ageInFiveYear: age + 5,
+      favNumber: Math.random(),
+      imgLink:
+        "https://learntalk.org/photos/blog/header_photo/226/Learntalk_BlogPosts_02-19-2018_Some_Any.jpg",
+    };
+  },
+  methods: {
+    ranNum() {
+      const ranNum = Math.random();
+      return ranNum;
+    },
+  },
+});
+
+app.mount("#assignment");
