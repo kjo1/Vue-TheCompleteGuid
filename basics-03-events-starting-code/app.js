f4f7f15604248d6fe30643250f002c05b7bd1376
kjo <kjo0@outlook.com>
2020-12-09 20:34:27 -0500

    24. Exploring Event Modifires

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -3,6 +3,7 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
+      confirmedName: "",
     };
   },
   methods: {
@@ -16,6 +17,12 @@ const app = Vue.createApp({
     },
     setName(event, lastName) {
       this.name = event.target.value + " " + lastName;
+    },
+    submitForm() {
+      alert("Submitted!");
+    },
+    confirmInput() {
+      this.confirmedName = this.name;
     },
   },
 });
