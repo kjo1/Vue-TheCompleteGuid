3f848c5324432ab88be968483312ba090d8d5e3c
kjo <kjo0@outlook.com>
2020-12-13 12:33:23 -0500

    29. Working with Watchers

--- basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/app.js
@@ -2,8 +2,35 @@ const app = Vue.createApp({
     data() {
         return {
             counter: 0,
-            name: ''
+            name: '',
+            lastName: '',
+            //fullname: ''
         };
+    },
+    watch: {
+        counter(value) {
+            if (value > 50) {
+                const that = this;
+                setTimeout(function () {
+                    this.counter = 0;
+                }, 2000);
+                this.counter = 0;
+            }
+        },
+        //     name(value) {
+        //         if (value === '') {
+        //             this.fullname = '';
+        //         } else {
+        //             this.fullname = value + ' ' + this.lastName;
+        //         }
+        //     },
+        //     lastName(value) {
+        //         if (value === '') {
+        //             this.fullname = '';
+        //         } else {
+        //             this.fullname = this.name + ' ' + value;
+        //         }
+        //     }
     },
     methods: {
         setName(event, lastName) {
@@ -18,7 +45,7 @@ const app = Vue.createApp({
         },
         resetInput() {
             this.name = '';
-        }
+        },
         // outputFullname() {
         // console.log('Running again...');
         //     if (this.name === '') {
@@ -29,12 +56,12 @@ const app = Vue.createApp({
     computed: {
         fullname() {
             console.log('Running again...');
-            if (this.name === '') {
+            if (this.name === '' || this.lastName === '') {
                 return '';
             }
-            return this.name + ' ' + 'Lastname';
-        }
-    }
+            return this.name + ' ' + this.lastName;
+        },
+    },
 });
 
 app.mount('#events');
