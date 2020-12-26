6c233435092869b21045cdcd75093b49386d39b5
kjo <kjo0@outlook.com>
2020-12-25 20:39:23 -0500

    43. Rendering Lists of Data

--- Section 3/lists-cond-01-starting-setup/app.js
+++ Section 3/lists-cond-01-starting-setup/app.js
@@ -7,7 +7,7 @@ const app = Vue.createApp({
     },
     methods: {
         addGoal() {
-            this.goals.push(this.enteredGoalValue);
+            this.goals.push(this.inputGoalValue);
         },
     },
 });
