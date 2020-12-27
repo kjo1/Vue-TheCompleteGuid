2eb4ea4a0d182942d449707605bef9a445ea6cd6
kjo <kjo0@outlook.com>
2020-12-26 22:30:14 -0500

    Assignment 5: Coditional Content & Lists after watching the solution

--- Section 3/lists-cond-assignment-problem/app.js
+++ Section 3/lists-cond-assignment-problem/app.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
         return {
             inputTask: '',
             tasks: [],
-            show: false,
+            show: false, //taskListIsVisible
+            // Variable names should be more self explanatory
             btnLabel: 'Hide',
             status: 'Shown',
         };
@@ -12,8 +13,10 @@ const app = Vue.createApp({
         addTask() {
             this.tasks.push(this.inputTask);
         },
+
         toggle() {
             this.show = !this.show;
+            // I did this way
             if (!this.show) {
                 this.btnLabel = 'Hide';
                 this.status = 'Shown';
@@ -21,6 +24,12 @@ const app = Vue.createApp({
                 this.btnLabel = 'Show';
                 this.status = 'Hidden';
             }
+        },
+    },
+    // better way to do the button caption
+    computed: {
+        btnCaption() {
+            return this.show ? 'Show List' : 'Hide List';
         },
     },
 });
