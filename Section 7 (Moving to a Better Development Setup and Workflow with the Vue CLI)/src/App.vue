07308362f0a1735fe3a39ad5e079c324e74125ce
kjo <kjo0@outlook.com>
2021-01-05 22:03:42 -0500

    1~10. Creating a Basic Vue App

---
+++ Section 7 (Moving to a Better Development Setup and Workflow with the Vue CLI)/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <section>
+        <h2>My Friends</h2>
+        <ul>
+            <li></li>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            friends: [
+                {
+                    id: 'manuel',
+                    name: 'Manuel Lorenz',
+                    phone: '123 456 7890',
+                    email: 'manuel@localhost.com',
+                },
+                {
+                    id: 'julie',
+                    name: 'Julie Jones',
+                    phone: '987 654 3210',
+                    email: 'julie@localhost.com',
+                },
+            ],
+        };
+    },
+};
+</script>
