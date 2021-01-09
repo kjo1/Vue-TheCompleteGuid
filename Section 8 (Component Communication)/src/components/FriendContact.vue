1a3b5697c306ef50bc7332ea10492a82279278c9
kjo <kjo0@outlook.com>
2021-01-09 15:32:50 -0500

    7. Emitting Custom Events (Child = Parent Communication)

--- Section 8 (Component Communication)/src/components/FriendContact.vue
+++ Section 8 (Component Communication)/src/components/FriendContact.vue
@@ -1,53 +1,56 @@
 <template>
-    <li>
-        <h2>{{ name }} {{ friendIsFavorite ? '(Favorite)' : '' }}</h2>
-        <button @click="toggleFavorite">Toggle Favorite</button>
-        <button @click="toggleDetails">Show Details</button>
-        <ul v-if="detailsAreVisible">
-            <li><strong>Phone:</strong> {{ phoneNumber }}</li>
-            <li><strong>Phone:</strong> {{ emailAddress }}</li>
-        </ul>
-    </li>
+  <li>
+    <h2>{{ name }} {{ isFavorite ? "(Favorite)" : "" }}</h2>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ phoneNumber }}</li>
+      <li><strong>Phone:</strong> {{ emailAddress }}</li>
+    </ul>
+  </li>
 </template>
 
 <script>
 export default {
-    // props: ['name', 'phoneNumber', 'emailAddress', 'isFavorite'],
-    props: {
-        name: {
-            type: String,
-            required: true,
-        },
-        phoneNumber: {
-            type: String,
-            required: true,
-        },
-        emailAddress: {
-            type: String,
-            required: true,
-        },
-        isFavorite: {
-            type: Boolean,
-            required: false,
-            default: false,
-            // validator: function(value) {
-            //     return value === '1' || value === '0';
-            // },
-        },
+  // props: ['name', 'phoneNumber', 'emailAddress', 'isFavorite'],
+  props: {
+    id: {
+      type: String,
+      required: true,
     },
-    data() {
-        return {
-            detailsAreVisible: false,
-            friendIsFavorite: this.isFavorite,
-        };
+    name: {
+      type: String,
+      required: true,
     },
-    methods: {
-        toggleDetails() {
-            this.detailsAreVisible = !this.detailsAreVisible;
-        },
-        toggleFavorite() {
-            this.friendIsFavorite = !this.friendIsFavorite;
-        },
+    phoneNumber: {
+      type: String,
+      required: true,
     },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+      // validator: function(value) {
+      //     return value === '1' || value === '0';
+      // },
+    },
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      this.$emit("toggle-favorite", this.id);
+    },
+  },
 };
 </script>
