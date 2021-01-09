38684833463bf06068ac9caa2e389f3e530ca041
kjo <kjo0@outlook.com>
2021-01-09 18:45:21 -0500

    catch up

--- Section 8 (Component Communication)/src/components/NewFriend.vue
+++ Section 8 (Component Communication)/src/components/NewFriend.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
    <form @submit.prevent="submitData">
       <div>
          <label>Name</label>
@@ -17,30 +16,10 @@
          <button>Add Contact</button>
       </div>
    </form>
-=======
-  <form @submit.prevent="submitData">
-    <div>
-      <label>Name</label>
-      <input type="text" v-model="enteredName" />
-    </div>
-    <div>
-      <label>Phone</label>
-      <input type="tel" v-model="enteredPhone" />
-    </div>
-    <div>
-      <label>E-Mail</label>
-      <input type="email" v-model="enteredEmail" />
-    </div>
-    <div>
-      <button>Add Contact</button>
-    </div>
-  </form>
->>>>>>> 43c546a2770bad4d20612cefa03dc495e607c516
 </template>
 
 <script>
 export default {
-<<<<<<< HEAD
    emits: ['add-contact'],
    data() {
       return {
@@ -56,20 +35,3 @@ export default {
    }
 };
 </script>
-=======
-  emits: ['add-contact'],
-  data(){
-     return {
-        enteredName: '',
-        enteredPhone: '',
-        enteredEmail: '',
-     }
-  },
-  methods: {
-     submitData(){
-        this.$emit('add-contact', this.enteredName, this.enteredPhone, this.enteredEmail)
-     }
-  }
-};
-</script>
->>>>>>> 43c546a2770bad4d20612cefa03dc495e607c516
